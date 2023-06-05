a794574f9644cbaf32f3a1084a99db5c65ae4828
sangkyu.kim <doodllllllll@gmail.com>
2023-06-05 16:26:26 +0900

    [feature] implement app

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "..\Project17/DeviceDriver.cpp"
+#include "..\Project17/App.cpp"
 
 using namespace std;
 using namespace testing;
@@ -32,7 +33,7 @@ TEST(TESTCaseName, ReadExceptionTest1)
 		.WillOnce(Return(0x2));
 
 	DeviceDriver driver(&mock_device);
-	EXPECT_THROW(driver.read(0x2),exception);
+	EXPECT_THROW(driver.read(0x2), exception);
 }
 
 TEST(TestCaseName, WriteSuccessTest1)
@@ -50,7 +51,36 @@ TEST(TestCaseName, WriteExceptionTest1)
 {
 	MockDevice mock_device;
 	EXPECT_CALL(mock_device, read(0x2)).WillRepeatedly(Return(0xFF));
-	
+
 	DeviceDriver driver(&mock_device);
 	EXPECT_THROW(driver.write(0x2, 0xAA), exception);
+}
+
+TEST(TestCaseName, AppReadAndPrintSuccess1)
+{
+	MockDevice mock_device;
+	EXPECT_CALL(mock_device, read(0x0)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, read(0x1)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, read(0x2)).WillRepeatedly(Return(0x1));
+	DeviceDriver driver(&mock_device);
+	Application apps(&driver);
+	apps.ReadAndPrint(0x0, 0x2);
+}
+
+TEST(TestCaseName, AppWriteAllSuccess1)
+{
+	MockDevice mock_device;
+	EXPECT_CALL(mock_device, read(0x0)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, write(0x0, 0x5));
+	EXPECT_CALL(mock_device, read(0x1)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, write(0x1, 0x5));
+	EXPECT_CALL(mock_device, read(0x2)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, write(0x2, 0x5));
+	EXPECT_CALL(mock_device, read(0x3)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, write(0x3, 0x5));
+	EXPECT_CALL(mock_device, read(0x4)).WillRepeatedly(Return(0x1));
+	EXPECT_CALL(mock_device, write(0x4, 0x5));
+	DeviceDriver driver(&mock_device);
+	Application apps(&driver);
+	apps.WriteAll(0x5);
 }
